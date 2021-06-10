ef19e4b3c5461bcb67a6185937fd2bf7b91f7353
Furkan Şimşek <83591761+Furkan-Simsek@users.noreply.github.com>
2021-06-10 12:13:27 +0300

    big change

---
+++ README.md
@@ -0,0 +1,7 @@
+##Calculator with simple interface
+
+Making a simple interface calculator with the PyQt5 module
+
+How many points out of 10 would you give, it is very important for me to tell the shortcomings and not make suggestions.
+
+-Furkan Şimşek
--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ class MainForm(QMainWindow):
           self.initUI()
      def initUI(self):
           self.lbl_sayi1 = QtWidgets.QLabel(self)
-          self.lbl_sayi1.setText('Sayı 1: ')
+          self.lbl_sayi1.setText('Enter number 1: ')
           self.lbl_sayi1.move(50,30)
 
           self.txt_sayi1 = QtWidgets.QLineEdit(self)
@@ -20,7 +20,7 @@ class MainForm(QMainWindow):
           self.txt_sayi1.resize(200,32)
 
           self.lbl_sayi2 = QtWidgets.QLabel(self)
-          self.lbl_sayi2.setText('Sayı 2: ')
+          self.lbl_sayi2.setText('Enter number 2: ')
           self.lbl_sayi2.move(50,80)
 
           self.txt_sayi2 = QtWidgets.QLineEdit(self)
@@ -28,27 +28,27 @@ class MainForm(QMainWindow):
           self.txt_sayi2.resize(200,32)
 
           self.btn_topla = QtWidgets.QPushButton(self)
-          self.btn_topla.setText("Toplama")
+          self.btn_topla.setText("Addition")
           self.btn_topla.move(150,130)
           self.btn_topla.clicked.connect(self.hesapla)
 
           self.btn_cikar = QtWidgets.QPushButton(self)
-          self.btn_cikar.setText("Çıkarma")
+          self.btn_cikar.setText("Subtraction")
           self.btn_cikar.move(150,170)
           self.btn_cikar.clicked.connect(self.hesapla)
 
           self.btn_carp = QtWidgets.QPushButton(self)
-          self.btn_carp.setText("Çarpma")
+          self.btn_carp.setText("Multiplication")
           self.btn_carp.move(150,210)
           self.btn_carp.clicked.connect(self.hesapla)
 
           self.btn_bol = QtWidgets.QPushButton(self)
-          self.btn_bol.setText("Bölme")
+          self.btn_bol.setText("Division")
           self.btn_bol.move(150,250)
           self.btn_bol.clicked.connect(self.hesapla)
 
           self.lbl_sonuc = QtWidgets.QLabel(self)
-          self.lbl_sonuc.setText('Sonuç: ')
+          self.lbl_sonuc.setText('Result: ')
           self.lbl_sonuc.move(150,290)
      def hesapla(self):
           sender = self.sender().text()
